ebd2c6cef39fd7797f71c9818794fbbff104eb62
slugbug432-sketch <slugbug432@gmail.com>
2026-04-05 17:40:24 -0500

    Fixed indentation and port configuration

--- app.py
+++ app.py
@@ -472,5 +472,11 @@ if __name__ == "__main__":
 
         db.create_all()
         create_default_users()
+    import os
 
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+
+app.run(
+    host="0.0.0.0",
+    port=port
+)
